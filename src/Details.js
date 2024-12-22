09367bef96049149c611a108398a17a6e7682a2d
Anuj Tyagi <76681167+anujtyagi07@users.noreply.github.com>
2024-12-22 21:03:11 +0530

    First Cmmit

---
+++ src/Details.js
@@ -0,0 +1,177 @@
+// Enter all your detials in this file
+// Logo images
+
+
+// Profile Image
+import profile from "./assets/profile.png";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import mongo from "./assets/techstack/mongo.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Anuj Tyagi",
+  tagline: "I build things for web",
+  img: profile,
+  about: `I am a highly motivated Software Development Engineer (SDE) with a strong foundation in Java, MERN Stack, and Data Structures. I completed my BTech in Computer Science and Engineering from MIET in 2023 and am currently pursuing advanced training in Java and MERN Stack through Coding Ninjas. My internship at Technobull India allowed me to gain practical experience and enhance my problem-solving skills in a professional environment. I have worked on diverse projects, including social media APIs, expense management systems, and a weather forecast app, showcasing my ability to build scalable and user-friendly solutions. With a passion for innovation, I am eager to make a positive impact through technology.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/anuj-tyagi-a1109b203/",
+  github: "https://github.com/anujtyagi07",
+  twitter: "https://twitter.com/",
+  instagram: "https://www.instagram.com/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Web Developer Intern",
+    Company: `Technobull India`,
+    Location: "Ghazabad",
+    Type: "Internship",
+    Duration: "Jan 2024 - Jun 2024",
+    About : "- Designed and developed responsive websites tailored to the needs of the hospitality industry, enhancing user experiences for clients and customers.\n\n- Collaborated with stakeholders to create features such as online booking systems, interactive menus, and event management tools.\n\n- Optimized website performance to ensure faster loading times, resulting in a 15% increase in user retention.\n\n- Worked on front-end development using **HTML**, **CSS**, **JavaScript**, **Bootstrap** and **React.js**, ensuring modern and visually appealing designs.\n\n- Integrated backend functionality using **Node.js** and **MongoDB** to support dynamic content and user interactions.\n\n- Resolved bugs and technical issues to ensure smooth website functionality and improve user satisfaction.\n\n- Gained valuable experience in understanding the technical requirements of the hospitality industry and delivering effective solutions.\n\n- Contributed to a team-oriented, agile development environment, meeting project deadlines and maintaining high-quality deliverables.",
+  }
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    "Position": "Coding Ninjas Fullstack Bootcamp",
+    "Company": "Coding Ninjas",
+    "Location": "Online",
+    "Type": "Full Time",
+    "Duration": "Jul 2019 - Jul 2023",
+    "About": "- Completed comprehensive training in Java and the MERN Stack to build robust and scalable web applications.\n\n- Gained proficiency in front-end technologies such as **HTML**, **CSS**, **JavaScript**, and **React.js**, creating interactive and responsive user interfaces.\n\n- Mastered backend development using **Node.js** and **Express.js** for server-side logic and API development.\n\n- Worked extensively with **MongoDB** for database management, implementing CRUD operations and optimizing data storage solutions.\n\n- Developed a strong foundation in **Java** programming, focusing on **object-oriented principles**, **data structures**, and **algorithmic problem-solving**.\n\n- Built multiple projects, including real-world applications, to apply theoretical knowledge to practical scenarios.\n\n- Collaborated with peers in an agile development environment, enhancing teamwork and meeting deadlines for project deliverables.\n\n- Refined debugging skills and resolved technical challenges to ensure application reliability and performance."
+  }
+  
+  
+,  
+  {
+    Position: "Bachelor in Computer Science and Engineering",
+    Company: `Meerut Institute of Engineering and and Technlogy`,
+    Location: "Meerut",
+    Type: "Full Time",
+    Duration: "Aug 2019 - Aug 2023",
+  },
+  {
+    Position: "12th",
+    Company: `S.F.D.A.V. Public School`,
+    Location: "Muzaffarnagar",
+    Type: "Full Time",
+    Duration: "2018 - 2019",
+  },
+  {
+    Position: "10th",
+    Company: `S.F.D.A.V. Public School`,
+    Location: "Muzaffarnagar",
+    Type: "Full Time",
+    Duration: "2016 - 2017",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  mongo: mongo,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Superhero Hunter App",
+    image: projectImage1,
+    description: `Begin your search for a favorite superhero by typing their name into the search bar, where suggestions will appear dynamically as you type. Click on the name of your preferred superhero from the suggestions to view detailed information about them. If you'd like to add a hero to your list of favorites, simply click on the "Add to favorites" option next to their name in the suggestions. You can also choose to unfavorite a hero directly from the suggestions list. To view your curated list of favorite superheroes, navigate to the "Favorite Heroes" page by clicking on the link in the top right corner of the home page. On this page, you'll find a comprehensive list of your favorite heroes, along with the option to remove any superhero from the list by selecting the respective removal option.`,
+    techstack: "HTML, CSS, Bootstrap , DOM, JavaScript",
+    previewLink: "https://anujtyagi07.github.io/SuperHero_Hunter",
+    githubLink: "https://github.com/anujtyagi07/SuperHero_Hunter",
+  },
+  {
+    title: "SkyCast - Interactive Weather App",
+    image: projectImage2,
+    description: `Welcome to SkyCast, a modern and intuitive weather forecasting app designed to keep you informed and prepared for any weather conditions. With its fully responsive design, SkyCast offers a seamless experience on all devices, making weather updates accessible anytime, anywhere. The app features dynamic weather icons that provide a quick and clear visualization of current conditions, along with detailed hourly and weekly forecasts to help you plan ahead confidently. Today’s Highlights section delivers essential metrics such as UV Index, Wind Speed, Sunrise & Sunset, Humidity, Visibility, and Air Quality. With global search functionality, you can easily check the weather in any location worldwide. SkyCast fetches accurate and real-time data from the Visual Crossing Weather API, offering precise insights into temperature, precipitation, wind, and more to ensure you’re always prepared for the day ahead.`,
+    techstack: "HTML , CSS, Bootstrap ,JavaScript",
+    //previewLink: "https://anujtyagi07.github.io/Weather-Predictor-App-/",
+    githubLink: "https://github.com/anujtyagi07/Weather-Predictor-App-",
+  },
+  {
+    title: "Carpooling App",
+    image: projectImage3,
+    description: `The carpooling app project is a full-stack web application built with **Node.js**, **Express.js**, and **MongoDB** for the backend, and **HTML**, **CSS**, and **React.js** for the frontend. The app allows users to create, join, and manage carpool rides efficiently. On the backend, APIs are implemented to handle user authentication, ride creation, and database management. The frontend provides an intuitive user interface, allowing users to easily browse and join available carpool rides. The project emphasizes seamless interaction between the front and back ends, utilizing dynamic features powered by React. With responsive design and optimized user experience, the app aims to promote eco-friendly transportation solutions.`,
+    techstack: "HTML , CSS , JavaScript , Reactjs , NodeJs , MongoDB , JWT",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "frontend Tourism web page",
+    image: projectImage4,
+    description: `The frontend tourism website project is built using **HTML**, **CSS**, and **Bootstrap** to create a visually appealing and user-friendly interface for users interested in exploring travel destinations. The website features a clean and modern design with sections highlighting popular tourist spots, travel tips, and booking options. By leveraging **Bootstrap's responsive grid system**, the website ensures compatibility across various devices and screen sizes, providing an optimal browsing experience. Custom CSS enhances the overall look and feel, while **JavaScript** and DOM manipulation add interactivity, such as image sliders and dynamic content loading. The project emphasizes accessibility, fast loading times, and a seamless navigation experience for users.`,
+    techstack: "HTML , CSS, Bootsrap",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com/anujtyagi07/Tourism_Website_FrontEnd",
+  },
+  {
+    title: "Stopwatch",
+    image: projectImage5,
+    description: `The Stopwatch application offers straightforward functionality:
+
+Start Button: Clicking the start button initiates the timer. If the timer is at 0, it begins from the start; otherwise, it resumes from where it was last stopped.
+
+Stop Button: Pressing the stop button halts the timer.
+
+Reset Button: Clicking the reset button stops the timer (if running) and sets it back to 0.`,
+    techstack: "HTML , CSS , JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com/anujtyagi07/Stopwatch-Webpage",
+  },
+  {
+    title: "Ludo King",
+    image: projectImage6,
+    description: `Ludo King landing page`,
+    techstack: "HTML , CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com/anujtyagi07/LudoKing",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "anujtyagi0720@gmail.com",
+  phone: "+919627077507",
+};
